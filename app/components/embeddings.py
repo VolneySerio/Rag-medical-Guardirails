b8e1289588889acd5800c4ba7b2550151c4a92e2
volney <volneyserio@gmail.com>
2026-09-18 12:02:38 -0300

    Refactor embedding and vector store functions for improved error handling and logging

--- app/components/embeddings.py
+++ app/components/embeddings.py
@@ -8,12 +8,28 @@ logger = get_logger(__name__)
 
 # Arranjar embedding model
     #Tentar 
-        # loggar a inicialização
-        # instancia o modelo. (Selecionar modelo)
+        # loggar a inicialização 
+        # instancia o modelo. (Selecionar modelo) - "sentence-transformers/all-MiniLM-L6-v2"
         # Logga o loading do modelo
         # retorna o modelo
 
     #exception handler
 
 def get_embedding_model():
-    return 
+    try:
+        
+        logger.info("Buscando embeddings do modelo no Hugging Face")
+
+        model = HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-L6-v2")
+
+        logger.info("Modelo carregado com sucesso")
+
+        return model
+
+    except Exception as e:
+        error_msg = CustomException("Error ocorreu enquanto carregava o embedding do modelo : ", e)
+        logger.error(str(error_msg))
+
+        raise error_msg
+
+    #return 
--- app/components/vector_store.py
+++ app/components/vector_store.py
@@ -5,6 +5,30 @@ from app.components.embeddings import get_embedding_model
 from app.common.logger import get_logger
 from app.common.custom_expection import CustomException
 
+from app.config.config import DB_FAISS_PATH
+
 
 def load_vector_store():
-    
+    #tentar
+        #puxar o embedding model
+
+        #Se existir o path do db
+            #retornar o Fais.load_local(path, o modelo e autorizar umas paradas doidas de perigo)
+        #Se não
+            #Loggar informação
+    #excep
+    # Mensagem de error
+    return
+
+def save_vector_store(text_chunkss):
+    #tentar
+        #checar se existe o text_chunks
+        #Subir o error
+    #loggar se der certo
+    # modelo embedding
+    #banco de dados FAIIS.from documents
+    #loggar informação salva no banco
+    #retornar DB
+    #excep 
+        #mensagem de error
+    return 
